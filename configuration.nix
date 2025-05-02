b09b15c316efef5d59c80164ddd16bed9786dbe0
Balázs Marcell <KoegLike@proton.me>
2025-05-02 10:22:46 +0300

    removed incorrect tmux config

--- configuration.nix
+++ configuration.nix
@@ -147,10 +147,7 @@
   programs.tmux = {
     enable = true;
     clock24 = true;
-    extraConfig = ''
-      # used for less common options, intelligently combines if defined in multiple places.
-         ...
-    '';
+    extraConfig = '' '';
   };
 
   # Enable the OpenSSH daemon.
